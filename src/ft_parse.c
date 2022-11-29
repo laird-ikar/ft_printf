a845fac03aba64143940b2b4fd46bafb7d9646fa
laird-ikar <bguyot@student.42mulhouse.fr>
2022-11-29 10:09:34 +0100

    get flag + parse flag

--- src/ft_parse.c
+++ src/ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 07:41:11 by bguyot            #+#    #+#             */
-/*   Updated: 2022/11/29 08:58:59 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/11/29 10:06:30 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ void	ft_parse(t_list *module, va_list args)
 	while (module)
 	{
 		flag = get_flag(module->content);
-		if (flag.to_parse)
-			module->content = parse_arg(args, flag);
+		if (flag.conv_type)
+			module->content = expend_arg(args, flag);
 		module = module->next;
 	}
 	(void) args;
@@ -33,15 +33,36 @@ void	ft_parse(t_list *module, va_list args)
 t_flag	get_flag(char *token)
 {
 	t_flag	ret;
+	char	conv;
 
-	ret.to_parse = 0;
+	conv = token[ft_strlen(token) - 1];
+	ft_bzero(&ret, sizeof(t_flag));
+	ret.precision = -1;
+	if (token[0] != '%' || !is_conversion(conv))
+	{
+		ret.conv_type = 0;
+		return (ret);
+	}
+	ret.conv_type = conv;
+	while (is_flag(*(++token)))
+		parse_flag(&ret, *token);
+	ret.min_width = ft_atoi(token);
+	token = ft_strnstr(token, ".", ft_strlen(token));
+	if (token++)
+		ret.precision = ft_atoi(token);
 	return (ret);
-	(void) token;
 }
 
-char	*parse_arg(va_list args, t_flag flag)
+void	parse_flag(t_flag *flag, char c)
 {
-	(void) args;
-	(void) flag;
-	return (NULL);
+	if (c == '#')
+		flag->do_alt_form = 1;
+	else if (c == '+')
+		flag->positive_sign = '+';
+	else if (c == ' ')
+		flag->positive_sign = ' ';
+	else if (c == '0')
+		flag->padding_type = '0';
+	else if (c == '-')
+		flag->padding_type = '-';
 }
