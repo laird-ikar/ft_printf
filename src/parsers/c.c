ccb8d35efad78b74b69ab7567b7a85cbf77b7cd4
laird-ikar <bguyot@student.42mulhouse.fr>
2022-11-30 13:21:03 +0100

    char func (v1)

--- src/parsers/c.c
+++ src/parsers/c.c
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 08:02:03 by bguyot            #+#    #+#             */
-/*   Updated: 2022/11/30 08:49:48 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/11/30 09:52:41 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 char	*c(va_list lst, t_flag flag)
 {
-	return (NULL);
-	(void) lst;
+	char	*ret;
+	char	arg;
+
+	ret = calloc(2, sizeof (char));
+	arg = va_arg(lst, int);
+	ret[0] = arg % 256;
+	return (ret);
 	(void) flag;
 }
