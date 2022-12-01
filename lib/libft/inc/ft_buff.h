5de84fa1fd7b3db5941ac127b2ece577694fe26d
laird-ikar <bguyot@student.42mulhouse.fr>
2022-12-01 09:44:13 +0100

    cleaning libft mistakes + make ft_bufclear compatible w/ ft_lstclear

--- lib/libft/inc/ft_buff.h
+++ lib/libft/inc/ft_buff.h
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 07:42:27 by bguyot            #+#    #+#             */
-/*   Updated: 2022/12/01 08:40:12 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/12/01 09:39:11 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # ifndef T_BUFF
 #  define T_BUFF
 
-typedef struct s_buffer
+typedef struct s_buff
 {
 	char	*data;
 	size_t	len;
@@ -32,6 +32,6 @@ typedef struct s_buffer
 t_buff	ft_bufcat(t_buff a, t_buff b);
 
 /* free data and set len to 0 */
-void	ft_bufclear(t_buff *buf);
+void	ft_bufclear(void *buf);
 
 #endif
--- lib/libft/src/ft_buff/ft_bufclear.c
+++ lib/libft/src/ft_buff/ft_bufclear.c
@@ -6,14 +6,14 @@
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 07:58:18 by bguyot            #+#    #+#             */
-/*   Updated: 2022/12/01 08:39:14 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/12/01 09:39:14 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/ft_buff.h"
 
-void	ft_bufclear(t_buff *buf)
+void	ft_bufclear(void *buf)
 {
-	free(buf->data);
-	buf->len = 0;
+	free(((t_buff *) buf)->data);
+	((t_buff *) buf)->len = 0;
 }
