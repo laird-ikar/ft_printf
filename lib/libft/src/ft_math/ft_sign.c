f99f91f7f6ee80ad8cb341e39e9f98b7f134fd11
laird-ikar <bguyot@student.42mulhouse.fr>
2022-12-02 10:10:53 +0100

    improve libft

--- lib/libft/src/ft_math/ft_sign.c
+++ lib/libft/src/ft_math/ft_sign.c
@@ -1,31 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_math.h                                          :+:      :+:    :+:   */
+/*   ft_sign.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/28 08:41:40 by bguyot            #+#    #+#             */
-/*   Updated: 2022/12/02 09:51:55 by bguyot           ###   ########.fr       */
+/*   Created: 2022/12/02 09:50:51 by bguyot            #+#    #+#             */
+/*   Updated: 2022/12/02 09:51:29 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_MATH_H
-# define FT_MATH_H
-
-/* Return absolute value of a */
-int	ft_abs(int a);
-
-/* Return the squareroot of nb */
-int	ft_sqrt(int nb);
-
-/* Return the max between a and b */
-int	ft_max(int a, int b);
-
-/* Return the min between a and b */
-int	ft_min(int a, int b);
-
-/* Return the sign of x*/
-int	ft_sign(int x);
-
-#endif
+int	ft_sign(int x)
+{
+	if (x < 0)
+		return (-1);
+	if (x > 0)
+		return (1);
+	return (0);
+}
