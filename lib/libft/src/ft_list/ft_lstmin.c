b815e515bd5b013d2b02435dbcc4cb3d9677da45
laird-ikar <bguyot@student.42mulhouse.fr>
2022-12-06 09:49:09 +0100

    updating libft to fix old error nver catched

--- lib/libft/src/ft_list/ft_lstmin.c
+++ lib/libft/src/ft_list/ft_lstmin.c
@@ -6,13 +6,13 @@
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 14:26:54 by bguyot            #+#    #+#             */
-/*   Updated: 2022/11/28 09:02:23 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/12/06 09:48:35 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/ft_list.h"
 
-int	ft_lstmaxint(t_list *lst)
+int	ft_lstminint(t_list *lst)
 {
 	int	min;
 
