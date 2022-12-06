a67c190577817e849df6cb5227a9ddd76bf4c693
laird-ikar <bguyot@student.42mulhouse.fr>
2022-12-06 09:49:22 +0100

    fixing # of x and X

--- src/parsers/great_x.c
+++ src/parsers/great_x.c
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 08:49:35 by bguyot            #+#    #+#             */
-/*   Updated: 2022/12/06 09:30:02 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/12/06 09:40:12 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,17 @@ t_buff	*great_x(va_list lst, t_flag *flag)
 {
 	t_buff			*ret;
 	unsigned int	data;
+	char			*tmp;
 
 	ret = ft_calloc(1, sizeof (t_buff));
 	data = va_arg(lst, unsigned);
 	ret->data = ft_utoa_base(data, "0123456789ABCDEF");
+	if (data && flag->do_alt_form)
+	{
+		tmp = ft_strjoin("0X", ret->data);
+		free(ret->data);
+		ret->data = tmp;
+	}
 	ret->len = ft_strlen(ret->data);
 	update_zero_flag(flag, ret);
 	return (ret);
--- src/parsers/x.c
+++ src/parsers/x.c
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 08:50:37 by bguyot            #+#    #+#             */
-/*   Updated: 2022/12/06 09:30:05 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/12/06 09:39:53 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,18 @@
 t_buff	*x(va_list lst, t_flag *flag)
 {
 	t_buff			*ret;
+	char			*tmp;
 	unsigned int	data;
 
 	ret = ft_calloc(1, sizeof (t_buff));
 	data = va_arg(lst, unsigned);
 	ret->data = ft_utoa_base(data, "0123456789abcdef");
+	if (data && flag->do_alt_form)
+	{
+		tmp = ft_strjoin("0x", ret->data);
+		free(ret->data);
+		ret->data = tmp;
+	}
 	ret->len = ft_strlen(ret->data);
 	update_zero_flag(flag, ret);
 	return (ret);
